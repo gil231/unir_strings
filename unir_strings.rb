3736d33f5fa69feec5879e798d1424a77ab18b3b
gil <gil@MacBook-Pro-de-gil.local>
2016-09-06 16:17:29 -0600

    unir_strings

---
+++ unir_strings.rb
@@ -0,0 +1,10 @@
+def sum_words (name, l_name)
+	puts "Hola, #{name} #{l_name} fuiste elegido para participar"
+end
+ sum_words("Gil", "Martinez")
+ sum_words("Dany", "Robles")
+ sum_words("Carlos", "González")
+ sum_words("Fer", "Espejel")
+ sum_words("Sandy", "Torres")
+ sum_words("Abel", "Hernández")
+
